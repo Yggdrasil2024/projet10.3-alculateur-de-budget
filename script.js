c5e43e30dbbdd354f05be2a3321718cdf474bcc1
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-08-15 19:18:26 +0100

    feat: l'interface reagit maintenant il ne reste plus que la deletion

--- script.js
+++ script.js
@@ -1,23 +1,99 @@
-//constantes du DOM
+//constantes du script
 
 /* --- on load les operations si elles existent --- */
-let operations = localStorage.getItem("operation-list") || [];
+let operations = JSON.parse(localStorage.getItem("operation-list")) || [];
 
 /* --- element du formulaire --- */
 const form = document.getElementById("operation_formular");
 const operationTitle = document.getElementById("operation_title");
 const operationAmount = document.getElementById("operation_amount");
 const operationType = document.getElementById("operation_type");
+const operationList = document.getElementById("operations_list");
 
 /* --- les KPIs --- */
-const balance = document.getElementById("balance");
-const tatalIncome = document.getElementById("earnings");
-const totalSpent = document.getElementById("expenses");
+const balanceItem = document.getElementById("balance");
+const incomeItem = document.getElementById("earnings");
+const expenseItem = document.getElementById("expenses");
 
-const renderUI = () => {};
+/* --- dans la zone des operation --- */
+const badge = document.getElementById("sum-badge");
+
+/**
+ * fonction pour formatter la monnaie
+ *
+ * @param {number} amount - somme à formatter
+ * @returns {string} - somme formatté
+ */
+const currencyFormatter = (amount) => {
+  return new Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "XAF",
+  }).format(amount);
+};
+
+const renderUI = () => {
+  let earnedSum = 0;
+  let expensesSum = 0;
+  operationList.innerHTML = "";
+
+  if (operations.length === 0) {
+    operationList.innerHTML = `<p class="empty_operations">Aucune operation enregistré pour l'instant</p>`;
+  }
+
+  //affichage de chage element d'operation
+  operations.forEach((item) => {
+    const earning = item.type == 2;
+    const amount = item.amount;
+
+    //on ressence les depense et gains
+    if (earning) {
+      earnedSum += amount;
+    } else {
+      expensesSum += amount;
+    }
+
+    if (operations.length === 0) {
+    }
+    //la div pour les operations
+    const listItem = document.createElement("div");
+    listItem.classList.add("operation__item");
+    listItem.innerHTML = `<div class="operation__item__description">
+                                <div class="operation__item__title ${earning ? "dot-earning" : "dot-expense"}">
+                                    ${item.title}
+                                </div>
+                                <div class="operation__item__amt">
+                                    <span class="${earning ? "income" : "expense"}">${earning ? "+" : "-"}${currencyFormatter(amount)}</span>
+                                    <span class="badge ${earning ? "badge-incomes" : "badge-expenses"}">${earning ? "Revenu" : "Dépense"}</span>
+                                </div>
+                            </div>
+                            <div class="operation__item__action"><button type="button"><img
+                                        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXgtaWNvbiBsdWNpZGUteCI+PHBhdGggZD0iTTE4IDYgNiAxOCIvPjxwYXRoIGQ9Im02IDYgMTIgMTIiLz48L3N2Zz4="
+                                        alt="effacer">
+                                </button></div>
+                        </div>`;
+
+    operationList.appendChild(listItem);
+  });
+
+  //badge pour compter les operation
+  badge.textContent = `${operations.length} operation${operations.length > 1 ? "s" : ""}`;
+
+  //on s'occupe du contenu de la dashboard
+  balanceItem.textContent = `${currencyFormatter(earnedSum - expensesSum)}`;
+  incomeItem.textContent = `${currencyFormatter(earnedSum)}`;
+  expenseItem.textContent = `${currencyFormatter(expensesSum)}`;
+
+  //enregistrement des operation courante dans localstorage
+  localStorage.setItem("operation-list", JSON.stringify(operations));
+};
 
 const addToOperations = (operation) => {
-  operations.unshift(operation);
+  try {
+    operations.unshift(operation);
+    return true;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 /**
@@ -56,8 +132,12 @@ form.addEventListener("submit", (event) => {
 
   const f_title = operationTitle.value;
   const f_amount = parseFloat(operationAmount.value);
-  const f_type = operationType.value;
+  const f_type = Number(operationType.value);
 
+  if (!f_title || isNaN(f_amount) || !f_type || f_amount <= 0) {
+    alert("veuiller remplir correctement le formulaire");
+    return;
+  }
   //nouvelle operation
   const newOperation = {
     id: idGenerator(),
@@ -69,4 +149,10 @@ form.addEventListener("submit", (event) => {
   //ajout dans les les operations
   addToOperations(newOperation);
   form.reset();
+
+  //on met à jour l'interface
+  renderUI();
 });
+
+//on charge l'interface depuis donné
+renderUI();
